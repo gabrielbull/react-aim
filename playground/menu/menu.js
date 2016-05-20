55ce35421855cb122f8dff24c253057f5123bbd4
Gabriel Bull <me@gabrielbull.com>
2016-05-20 15:42:47 -0400

    Fixed menu style

--- playground/menu/menu.js
+++ playground/menu/menu.js
@@ -13,13 +13,15 @@ const style = {
 class Menu extends Component {
   render() {
     return (
-      <ul style={style}>
-        <Item name="item 1"/>
-        <Item name="item 2"/>
-        <Item name="item 3"/>
-        <Item name="item 4"/>
-        <Item name="item 5"/>
-      </ul>
+      <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+        <ul style={style}>
+          <Item name="item 1"/>
+          <Item name="item 2"/>
+          <Item name="item 3"/>
+          <Item name="item 4"/>
+          <Item name="item 5"/>
+        </ul>
+      </div>
     );
   }
 }
--- src/menu.js
+++ src/menu.js
@@ -3,68 +3,6 @@ import React, { Component, PropTypes } from 'react';
 export default function (...args) {
   return function (WrappedComponent) {
     return class extends Component {
-      static childContextTypes = {
-        bufferMenu_Menu: PropTypes.object
-      };
-
-      getChildContext() {
-        return {
-          bufferMenu_Menu: this
-        };
-      }
-
-      current;
-
-      componentDidMount() {
-        document.addEventListener('mousemove', this.handleMouseLeft)
-      }
-
-      componentWillUnmount() {
-        document.removeEventListener('mousemove', this.handleMouseLeft)
-      }
-
-      handleMouseLeft = e => {
-        if (this.current) {
-          const mouse = { x: e.pageX, y: e.pageY };
-          if (!this.isMouseOnElement(mouse, this.current.element)) {
-            this.current.component.triggerMouseLeave();
-            this.current = null;
-          }
-        }
-      };
-
-      handleMouseMove(component, element, mouse, target) {
-        if (!this.current && this.isMouseOnElement(mouse, element)) {
-          return this.makeCurrent(component, element, mouse, target);
-        }
-
-        if (this.current && this.current.component === component) {
-          if (!this.isMouseOnElement(mouse, element)) {
-            component.triggerMouseLeave();
-            this.current = null;
-          }
-          return null;
-        }
-
-        if (!this.current || !this.current.target) {
-          if (this.isMouseOnElement(mouse, element)) {
-            return this.makeCurrent(component, element, mouse, target);
-          }
-        }
-      }
-
-      isMouseOnElement(mouse, element) {
-        const rect = element.getBoundingClientRect();
-        return (mouse.x >= rect.left && mouse.x <= rect.left + rect.width) &&
-          (mouse.y >= rect.top && mouse.y <= rect.top + rect.height);
-      }
-
-      makeCurrent(component, element, mouse, target) {
-        component.triggerMouseEnter();
-        if (this.current) this.current.component.triggerMouseLeave();
-        this.current = { component, element, mouse, target };
-      }
-
       render() {
         return <WrappedComponent {...this.props}/>;
       }
