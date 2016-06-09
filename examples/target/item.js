b7ce97cd5325d78101eb6a559197d1aa2bb6ea0b
Gabriel Bull <me@gabrielbull.com>
2016-06-09 15:14:03 -0400

    Targets are now actually good looking

--- examples/target/item.js
+++ examples/target/item.js
@@ -1,6 +1,27 @@
 import React, { Component, PropTypes } from 'react';
 import { target } from 'react-aim';
 
+const styles = {
+  container: {
+    position: 'absolute',
+    width: '122px',
+    height: '142px'
+  },
+  progress: {
+    backgroundColor: 'rgba(0, 0, 0, .1)',
+    border: '1px solid rgba(0, 0, 0, .2)',
+    height: '8px',
+    width: '120px',
+    position: 'relative',
+    marginTop: '10px'
+  },
+  bar: {
+    width: '0%',
+    height: '8px',
+    backgroundColor: '#76e542'
+  }
+};
+
 @target(
   {
     mouseEnter: (props, component) => {
@@ -35,8 +56,8 @@ class Item extends Component {
   }
 
   componentWillMount() {
-    const maxX = window.innerWidth - 122 + 1;
-    const maxY = window.innerHeight - 122 + 1;
+    const maxX = window.innerWidth - parseInt(styles.container.width) + 1;
+    const maxY = window.innerHeight - parseInt(styles.container.height) + 1;
 
     this.setState({
       x: Math.floor(Math.random() * (maxX + 1)),
@@ -55,8 +76,8 @@ class Item extends Component {
   }
 
   resize = () => {
-    const maxX = window.innerWidth - 122 + 1;
-    const maxY = window.innerHeight - 122 + 1;
+    const maxX = window.innerWidth - parseInt(styles.container.width) + 1;
+    const maxY = window.innerHeight - parseInt(styles.container.height) + 1;
 
     this.setState({
       x: 1 / this.state.maxX * maxX * this.state.x,
@@ -67,32 +88,35 @@ class Item extends Component {
   };
 
   render() {
-    const style = {
-      position: 'absolute',
+    const containerStyle = {
+      ...styles.container,
       top: this.state.y + 'px',
-      left: this.state.x + 'px',
-      width: '122px',
-      height: '122px',
-      backgroundColor: 'rgb(0, 0, 0)'
+      left: this.state.x + 'px'
     };
 
+    const barStyle = { ...styles.bar };
+
+    let targetColor = '#000000';
+
     if (this.state.over) {
-      style.backgroundColor = 'rgb(255, 100, 100)';
-      style.boxShadow = '0 0 0 1px rgba(255, 0, 0, 1), 0 0 20px rgba(255, 100, 100, 0.4)';
+      barStyle.width = '100%';
+      targetColor = '#ff0000';
     } else if (this.state.distance) {
-      style.boxShadow = '0 0 0 1px rgba(255, 0, 0, 1)';
-      style.backgroundColor = 'rgb(' + Math.round(this.state.distance * 255) + ', ' + Math.round(this.state.distance * 100) + ', ' + Math.round(this.state.distance * 100) + ')';
+      barStyle.width = (this.state.distance * 100) + '%';
+      targetColor = 'rgb(' + Math.round(this.state.distance * 255) + ', 0, 0)';
     }
 
     return (
-      <div style={style}>
+      <div style={containerStyle}>
         <svg x="0px" y="0px" width="122px" height="122px" viewBox="0 0 122 122">
           <path
+            fill={targetColor}
             d="M61,0C27.4,0,0,27.4,0,61c0,33.6,27.4,61,61,61c33.6,0,61-27.4,61-61C122,27.4,94.6,0,61,0z M111.8,65.6
 	c-2.2,24.5-21.6,43.9-46.1,46.2l-3.3,0.3V91.3H60v20.8l-3.3-0.3C32,109.7,12.4,90.3,10.2,65.6l-0.3-3.3H31v-2.4H9.9l0.3-3.3
 	c2.1-24.8,21.7-44.4,46.5-46.5L60,9.9v21h2.4v-21l3.3,0.3c24.6,2.3,44,21.8,46.1,46.4l0.3,3.3H91.4v2.4h20.7L111.8,65.6z"
           />
         </svg>
+        <div style={styles.progress}><div style={barStyle}/></div>
       </div>
     );
   }
