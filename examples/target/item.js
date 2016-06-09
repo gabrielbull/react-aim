eee0941b7045714b7dea4fe6a782bfecdda68059
Gabriel Bull <me@gabrielbull.com>
2016-06-09 15:03:44 -0400

    Added random targets

--- examples/target/item.js
+++ examples/target/item.js
@@ -34,13 +34,45 @@ class Item extends Component {
     this.state = { distance: null, over: false }
   }
 
+  componentWillMount() {
+    const maxX = window.innerWidth - 122 + 1;
+    const maxY = window.innerHeight - 122 + 1;
+
+    this.setState({
+      x: Math.floor(Math.random() * (maxX + 1)),
+      y: Math.floor(Math.random() * (maxY + 1)),
+      maxX,
+      maxY
+    });
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.resize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize)
+  }
+
+  resize = () => {
+    const maxX = window.innerWidth - 122 + 1;
+    const maxY = window.innerHeight - 122 + 1;
+
+    this.setState({
+      x: 1 / this.state.maxX * maxX * this.state.x,
+      y: 1 / this.state.maxY * maxY * this.state.y,
+      maxX,
+      maxY
+    });
+  };
+
   render() {
     const style = {
       position: 'absolute',
-      top: this.props.top + 'px',
-      left: this.props.left + 'px',
-      width: this.props.width + 'px',
-      height: this.props.height + 'px',
+      top: this.state.y + 'px',
+      left: this.state.x + 'px',
+      width: '122px',
+      height: '122px',
       backgroundColor: 'rgb(0, 0, 0)'
     };
 
@@ -53,7 +85,15 @@ class Item extends Component {
     }
 
     return (
-      <div style={style}/>
+      <div style={style}>
+        <svg x="0px" y="0px" width="122px" height="122px" viewBox="0 0 122 122">
+          <path
+            d="M61,0C27.4,0,0,27.4,0,61c0,33.6,27.4,61,61,61c33.6,0,61-27.4,61-61C122,27.4,94.6,0,61,0z M111.8,65.6
+	c-2.2,24.5-21.6,43.9-46.1,46.2l-3.3,0.3V91.3H60v20.8l-3.3-0.3C32,109.7,12.4,90.3,10.2,65.6l-0.3-3.3H31v-2.4H9.9l0.3-3.3
+	c2.1-24.8,21.7-44.4,46.5-46.5L60,9.9v21h2.4v-21l3.3,0.3c24.6,2.3,44,21.8,46.1,46.4l0.3,3.3H91.4v2.4h20.7L111.8,65.6z"
+          />
+        </svg>
+      </div>
     );
   }
 }
--- examples/target/target.js
+++ examples/target/target.js
@@ -5,13 +5,21 @@ class Target extends Component {
   render() {
     return (
       <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
-        <Item top="300" left="200" width="300" height="100" color="red"/>
-        <Item top="500" right="200" width="100" height="100" color="red"/>
-        <Item top="100" left="0" width="100" height="100" color="red"/>
-        <Item top="600" left="1400" width="200" height="200" color="red"/>
-        <Item top="200" left="1500" width="200" height="200" color="red"/>
+        {this.renderItems()}
       </div>
     );
+  }
+
+  renderItems() {
+    let children = [];
+    for (let i = 0, len = (Math.floor(Math.random() * (20 + 1 - 10)) + 10); i < len; ++i) {
+      children.push(this.renderItem(i));
+    }
+    return children;
+  }
+
+  renderItem(index) {
+    return <Item key={index} top="300" left="200" width="300" height="100" color="red"/>;
   }
 }
 
